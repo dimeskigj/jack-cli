f32f87ece2151a9ecb4226754314c7b3eb0d4da3
dimeskigj <dimeskigj@gmail.com>
2025-05-26 23:31:25 +0200

    Configure tests and dependency locking

---
+++ src/test/kotlin/HelpTest.kt
@@ -0,0 +1,19 @@
+import org.factotum.main
+import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.Test
+
+class MainTest {
+    @Test
+    fun `when 'help' argument passed expect no exception`() {
+        assertDoesNotThrow {
+            main(arrayOf("--help"))
+        }
+    }
+
+    @Test
+    fun `when 'h' argument passed expect no exception`() {
+        assertDoesNotThrow {
+            main(arrayOf("--help"))
+        }
+    }
+}
